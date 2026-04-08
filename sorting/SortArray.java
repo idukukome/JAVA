88258629842293003a98826a79a2dd40dbaa5b1e
Iduku Kome <idukukome@gmail.com>
2026-04-08 11:18:59 +0100

    first commit

---
+++ sorting/SortArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 2, 9, 1, 7};
+
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted Array: " + Arrays.toString(numbers));
+    }
+}
